040411276f2bb14621c22a39a357fb5480c066de
geronimo-becker <gbecker@frba.utn.edu.ar>
2026-07-06 23:58:49 -0300

    Agregue Logs y try catch para lo asincronico

--- src/main/java/com/ddsi/climalert/service/EmailNotificationService.java
+++ src/main/java/com/ddsi/climalert/service/EmailNotificationService.java
@@ -1,11 +1,13 @@
 package com.ddsi.climalert.service;
 
 import com.ddsi.climalert.model.RegistroClimatico;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class EmailNotificationService {
 
@@ -17,21 +19,26 @@ public class EmailNotificationService {
 
   @Async
   public void enviarAlerta(RegistroClimatico registro) {
-    String[] destinatarios = {
-        "admin@clima.com",
-        "emergencias@clima.com",
-        "meteorologia@clima.com"
-    };
+    try {
+      String[] destinatarios = {
+          "admin@clima.com",
+          "emergencias@clima.com",
+          "meteorologia@clima.com"
+      };
 
-    SimpleMailMessage mensaje = new SimpleMailMessage();
-    mensaje.setTo(destinatarios);
-    mensaje.setSubject("ALERTA: Condiciones Climáticas Críticas");
-    mensaje.setText("Se han detectado condiciones climáticas inusuales:\n\n" +
-        "Detalle del clima:\n" +
-        "- Temperatura: " + registro.getTemperatura() + "°C\n" +
-        "- Humedad: " + registro.getHumedad() + "%\n" +
-        "- Fecha: " + registro.getFechaHora());
+      SimpleMailMessage mensaje = new SimpleMailMessage();
+      mensaje.setTo(destinatarios);
+      mensaje.setSubject("ALERTA: Condiciones Climáticas Críticas");
+      mensaje.setText("Se han detectado condiciones climáticas inusuales:\n\n" +
+          "Detalle del clima:\n" +
+          "- Temperatura: " + registro.getTemperatura() + "°C\n" +
+          "- Humedad: " + registro.getHumedad() + "%\n" +
+          "- Fecha: " + registro.getFechaHora());
 
-    mailSender.send(mensaje);
+      mailSender.send(mensaje);
+  } catch (Exception e) {
+    // Registra el error en los logs, no dejes que el hilo muera en silencio
+    log.error("Error actualizando clima de forma asíncrona: " + e.getMessage());
+  }
   }
 }
